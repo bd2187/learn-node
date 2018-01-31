aad71e1540bdaf27c8e2b2a75223968ab8987696
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-01-30 20:56:42 -0800

    Create hello.pug.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get('/', function(req, res) {
+  // const brandon = { name: 'brandon', occupation: 'web developer', age: 25 };
+  // console.log(req.query.name,  req.query.age);
+  // res.json(brandon); 
+  res.render('hello', {
+    name: req.query.name,
+    question: 'How are you?'
+  });
 });
+
+router.get('/reverse/:name', function( req, res ) {
+  const reversedName = [...req.params.name].reverse().join('');
+  const finalName = reversedName[0].toUpperCase() + reversedName.slice(1);
+
+  res.send(`Hello, ${finalName}`);
+});
+
 
 module.exports = router;
