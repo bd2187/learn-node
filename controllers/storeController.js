64417cc8774d1b148cf5047d16c336045e670bcd
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-02-02 17:47:44 -0800

    Create routes for get and post /add. Create addStore and createStore controllers. Render editStore.pugh when get request is sent to /add.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,49 @@
-exports.homePage = function(req, res) {
- 
+exports.homePage = function(req, res) { 
     res.render('index', {
       title: 'Home'
-    });
-    
+    });    
 };
+
+exports.addStore = function(req, res) {
+  res.render('editStore', {
+    title: 'Add Store'
+  });
+}
+
+exports.createStore = function(req, res) {
+  res.json(req.body);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// exports.addStore = function(req, res) {
+//   res.render('editStore', {
+//     title: 'Add Store'
+//   });
+// }
+
+// exports.createStore = function(req, res) {
+//   res.json(req.body);
+// }
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
-
-router.get('/reverse/:name', function( req, res ) {
-  const reversedName = [...req.params.name].reverse().join('');
-  const finalName = reversedName[0].toUpperCase() + reversedName.slice(1);
-
-  res.send(`Hello, ${finalName}`);
-});
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
